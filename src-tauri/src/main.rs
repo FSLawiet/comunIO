36f8180b66e574cffd2a187b41a0413f9c4eae9c
Fausto André <fslawliet@hotmail.com>
2023-03-06 10:50:48 -0300

    Ícones atualizados e icone de system tray

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,9 +3,31 @@
   windows_subsystem = "windows"
 )]
 
+use tauri::{Manager, SystemTray, SystemTrayEvent, SystemTrayMenu};
+
 fn main() {
+  let system_tray_menu = SystemTrayMenu::new();
+
   tauri::Builder::default()
     .plugin(tauri_plugin_sqlite::init())
+    .system_tray(SystemTray::new().with_menu(system_tray_menu))
+        .on_system_tray_event(|app, event| match event {
+            SystemTrayEvent::LeftClick {
+                position: _,
+                size: _,
+                ..
+            } => {
+                let window = app.get_window("main").unwrap();
+                // toggle application window
+                if window.is_visible().unwrap() {
+                    window.hide().unwrap();
+                } else {
+                    window.show().unwrap();
+                    window.set_focus().unwrap();
+                }
+            },
+            _ => {}
+        })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }

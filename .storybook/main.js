da2e76828ff27e2f341123336f42bffbab248ce0
Fausto André <fslawliet@hotmail.com>
2023-03-06 11:25:50 -0300

    Configuração Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,14 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-a11y',
+    '@storybook/addon-links',
+    '@storybook/addon-docs',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/vue3"
-}
+  framework: '@storybook/vue3',
+  features: {
+    interactionsDebugger: true,
+  },
+};
